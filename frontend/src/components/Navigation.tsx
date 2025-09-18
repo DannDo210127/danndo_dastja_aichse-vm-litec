96ce9a6dac8f2847233ff81fe733dddb2e717de5
Dominik Danner <40146720210127@litec.ac.at>
2025-09-18 10:02:08 +0200

    fixed structure

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,10 @@
+
+
+export function Navigation(){
+    return(
+        <div className="w-1/4 h-full color-foreground flex flex-row items-left justify-left p-4">
+           Navigation 
+        </div>
+    )
+    
+}
