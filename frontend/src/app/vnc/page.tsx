1e3dbac1a30e02196fd554e77dfddbdc93e2de0a
Severin Aichinger <40146720210188@litec.ac.at>
2026-01-22 10:34:20 +0100

    vnc scaling

--- frontend/src/app/vnc/page.tsx
+++ frontend/src/app/vnc/page.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useRef } from 'react';
+
 export default function VncPage() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
   return (
     <div className="flex flex-col bg-black w-full min-h-screen">
-      <iframe className="w-full h-dvh"
-        src="/novnc/vnc.html?host=192.168.61.129&port=6080&path=websockify&autoconnect=true"
+      <iframe 
+        ref={iframeRef}
+        className="w-full h-dvh"
+        src="/novnc/vnc.html?host=192.168.61.129&port=6080&resize=scale&reconnect=true&autoconnect=true"
         title="VNC Viewer"
       />
     </div>
